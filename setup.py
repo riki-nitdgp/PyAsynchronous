0ccc2eb9f628b0646e39f787cf5d81487ae3cc7c
riki-nitdgp <rkmondal41@gmail.com>
2020-09-22 23:34:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,45 @@
-from os import getcwd, path
+import os
 from setuptools import setup, find_packages
 
-if not path.dirname(__file__):
-    _dirname = getcwd()
-else:
-    _dirname = path.dirname(path.dirname(__file__))
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+BASE_DIR = os.path.join(os.path.dirname(__file__))
+
+with open(os.path.join(BASE_DIR, 'requirements.txt')) as f:
+    REQUIREMENTS = f.read()
 
 
-def read(name, default=None, debug=True):
-    try:
-        filename = path.join(_dirname, name)
-        with open(filename) as f:
-            return f.read()
-    except Exception as e:
-        err = "%s: %s" % (type(e), str(e))
-        if debug:
-            print(err)
-        return default
 
+URL = 'https://github.com/riki-nitdgp/PyAsync'
+README = "For more info, go to: {}".format(URL)
+VERSION = "1.1.0.dev"
 
-def lines(name):
-    txt = read(name)
-    return map(
-        lambda l: l.lstrip().rstrip(),
-        filter(lambda t: not t.startswith('#'), txt.splitlines() if txt else [])
-    )
-
-
-install_requires = [i for i in lines("requirements.txt") if '-e' not in i]
 
 setup(
     name='PyAsync',
-    version='1.0.0',
-    author='riki mondal',
+    version=VERSION,
+    description='PyAsync is lightweight and simple async web Framework based on python3.6+',
+    author='Riki Mondal',
     author_email='rkmondal41@gmail.com',
-    url='https://github.com/riki-nitdgp/PyAsync',
-    description='Python Async WebFramework',
-    packages=find_packages(exclude=['examples', 'tests', 'docs']),
-    install_requires=install_requires
+    long_description=README,
+    url=URL,
+    packages=find_packages(exclude=['test*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    setup_requires=REQUIREMENTS,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
 )
